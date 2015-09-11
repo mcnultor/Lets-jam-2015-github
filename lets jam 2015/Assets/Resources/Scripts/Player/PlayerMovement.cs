702e9db08d3dea8363f70dc2ac6b513c85a4b0ea
viscountess <venerable_viscountess@hotmail.com>
2015-09-11 22:22:48 +0100

    Implement basic X axis movement control
    Pressing A or <- will go left
    Pressing D or -> will go right

---
+++ lets jam 2015/Assets/Resources/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//Variables
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	void Start()
+	{
+	}
+
+	void FixedUpdate()
+	{
+		//Basic horizontal movement
+		float move = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		if (move > 0 && !facingRight)
+			FlipPlayer ();
+		else if (move < 0 && facingRight)
+			FlipPlayer ();
+	}
+
+	void FlipPlayer()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
